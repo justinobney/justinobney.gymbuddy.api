e90df5e6f648945a398a806ba06657b38900bb30
Justin Obney <justinobney@gmail.com>
2016-02-05 17:18:44 -0600

    feat(auth): adding auth controller

---
+++ justinobney.gymbuddy.api/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AutoMapper.QueryableExtensions;
+using justinobney.gymbuddy.api.Data.Users;
+using justinobney.gymbuddy.api.Requests.Generic;
+using justinobney.gymbuddy.api.Responses;
+using MediatR;
+
+namespace justinobney.gymbuddy.api.Controllers
+{
+    public class AuthController : ApiController
+    {
+        private readonly Mediator _mediator;
+
+        public AuthController(Mediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/Gyms
+        [ResponseType(typeof(ProfileListing))]
+        public IHttpActionResult Get()
+        {
+            var deviceId = Request.Headers.GetValues("device-id").FirstOrDefault();
+            Expression<Func<User, bool>> predicate = u => u.Devices.Any(d => d.DeviceId == deviceId);
+            var request = new GetAllByPredicateQuery<User> {Predicate = predicate};
+
+            var user = _mediator.Send(request)
+                .ProjectTo<ProfileListing>(MappingConfig.Config)
+                .FirstOrDefault();
+
+            return Ok(user);
+        }
+        
+    }
+}
