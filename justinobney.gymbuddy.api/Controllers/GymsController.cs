756fa6a670aafed08b700a0a40f54ff3cc8eafd6
Justin Obney <justinobney@gmail.com>
2016-02-11 13:51:17 -0600

    feat(mediatr): add more power to generic queries

--- justinobney.gymbuddy.api/Controllers/GymsController.cs
+++ justinobney.gymbuddy.api/Controllers/GymsController.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using AutoMapper.QueryableExtensions;
@@ -64,14 +62,8 @@ namespace justinobney.gymbuddy.api.Controllers
                 return Unauthorized();
             }
 
-            var users = _mediator.Send(new GetAllByPredicateQuery<User>
-            {
-                //FilterByGender(requestingUser, u)
-                Predicate = u => u.Gyms.Any(g => g.Id == id)
-                                 && u.FilterFitnessLevel <= CurrentUser.FitnessLevel
-                                 && u.FitnessLevel >= CurrentUser.FilterFitnessLevel
-                                 && u.Id != CurrentUser.Id
-            })
+            var request = new GetAllByPredicateQuery<User>(UserPredicates.RestrictMember(CurrentUser, id));
+            var users = _mediator.Send(request)
                     .ProjectTo<ProfileListing>(MappingConfig.Config);
 
             if (users == null)
@@ -81,60 +73,6 @@ namespace justinobney.gymbuddy.api.Controllers
 
             return Ok(users);
         }
-
-        //private bool FilterByGender(User requestingUser, User user)
-        //{
-        //    var LookingForOpposites = (int) requestingUser.FilterGender == (int) user.Gender &&
-        //                              (int) requestingUser.Gender == (int) user.FilterGender;
-
-        //    var MatchWhenRequestingBoth = requestingUser.FilterGender == GenderFilter.Both &&
-        //                                  (int)user.FilterGender == (int)requestingUser.Gender;
-
-        //    var MatchWhenUserBoth = user.FilterGender == GenderFilter.Both &&
-        //                                  (int)requestingUser.FilterGender == (int)user.Gender;
-
-        //    var BothAcceptBoth = user.FilterGender == GenderFilter.Both &&
-        //                         requestingUser.FilterGender == GenderFilter.Both;
-
-        //    return LookingForOpposites || MatchWhenRequestingBoth || MatchWhenUserBoth || BothAcceptBoth;
-        //}
         
-
-        // POST: api/Gyms
-        //[ResponseType(typeof(Gym))]
-        //public async Task<IHttpActionResult> PostGym(CreateGymCommand command)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    var user = await _mediator.SendAsync(command);
-        //    return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
-        //}
-
-
-        // PUT: api/Gyms/5
-        //[ResponseType(typeof(void))]
-        //public async Task<IHttpActionResult> PutGym(long id, UpdateGymCommand command)
-        //{
-        //    if (id != command.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    await _mediator.SendAsync(command);
-        //    return StatusCode(HttpStatusCode.NoContent);
-        //}
-
-        //TODO: Create generic delete command
-        // DELETE: api/Gyms/5
-        //[ResponseType(typeof(Gym))]
-        //public async Task<IHttpActionResult> DeleteGym(long id)
-        //{
-        //    var user = await _mediator.SendAsync(new DeleteGymCommand { Id = id });
-        //    return Ok(user);
-        //}
-
     }
 }
---
+++ justinobney.gymbuddy.api/Data/Users/UserPredicates.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using justinobney.gymbuddy.api.Enums;
+
+namespace justinobney.gymbuddy.api.Data.Users
+{
+    public class UserPredicates
+    {
+        public static Expression<Func<User, bool>> RestrictMember(User currentUser, long gymId)
+        {
+            Expression<Func<User, bool>> predicate = u => u.Gyms.Any(g => g.Id == gymId)
+                                                          && u.Id != currentUser.Id;
+
+            predicate = predicate.And(RestrictMemberByFitnessLevel(currentUser));
+            predicate = predicate.And(RestrictMemberByGender(currentUser));
+
+            return predicate;
+        }
+
+        public static Expression<Func<User, bool>> RestrictMemberByGender(User currentUser)
+        {
+            return user => (
+                ((int) currentUser.FilterGender == (int) user.Gender &&
+                 (int) currentUser.Gender == (int) user.FilterGender)
+                ||
+                (currentUser.FilterGender == GenderFilter.Both &&
+                 (int) user.FilterGender == (int) currentUser.Gender)
+                ||
+                (user.FilterGender == GenderFilter.Both &&
+                 (int) currentUser.FilterGender == (int) user.Gender)
+                ||
+                (user.FilterGender == GenderFilter.Both &&
+                 currentUser.FilterGender == GenderFilter.Both)
+                );
+        }
+
+        public static Expression<Func<User, bool>> RestrictMemberByFitnessLevel(User currentUser)
+        {
+            return user => user.FilterFitnessLevel <= currentUser.FitnessLevel
+                        && user.FitnessLevel >= currentUser.FilterFitnessLevel;
+        }
+    }
+}
