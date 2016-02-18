edb12caf331254bd6e440f1d307b3d9cf5ed9fc0
Justin Obney <justinobney@gmail.com>
2016-02-17 18:01:44 -0600

    feat(tests): setting up unit tests

--- justinobney.gymbuddy.api.tests/BaseTest.cs
+++ justinobney.gymbuddy.api.tests/BaseTest.cs
@@ -1,20 +1,26 @@
 ﻿using justinobney.gymbuddy.api.DependencyResolution;
-using justinobney.gymbuddy.api.Enums;
+using justinobney.gymbuddy.api.tests.DependencyResolution;
 using MediatR;
 using NUnit.Framework;
 using StructureMap;
 
 namespace justinobney.gymbuddy.api.tests
 {
+    [TestFixture]
     public class BaseTest
     {
-        public IMediator _mediator { get; set; }
+        protected IMediator Mediator;
 
-        [SetUp]
+        [OneTimeSetUp]
         public void SetUp()
         {
-            var container = new Container(new DefaultRegistry());
-            _mediator = container.GetInstance<IMediator>();
+            var registry = new Registry();
+            registry.IncludeRegistry<DefaultRegistry>();
+            registry.IncludeRegistry<GenericCrudRegistry>();
+            registry.IncludeRegistry<FakeEntityFrameworkRegistry>();
+
+            var container = new Container(registry);
+            Mediator = container.GetInstance<IMediator>();
         }
     }
 }
---
+++ justinobney.gymbuddy.api.tests/DependencyResolution/FakeEntityFrameworkRegistry.cs
@@ -0,0 +1,20 @@
+﻿using justinobney.gymbuddy.api.Data.Users;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace justinobney.gymbuddy.api.tests.DependencyResolution
+{
+    public class FakeEntityFrameworkRegistry : Registry
+    {
+        public FakeEntityFrameworkRegistry()
+        {
+            Scan(scan =>
+            {
+                scan.TheCallingAssembly();
+                scan.AssemblyContainingType(typeof(User));
+                scan.Convention<FakeEntityFrameworkConvention>();
+                scan.WithDefaultConventions();
+            });
+        }
+    }
+}
