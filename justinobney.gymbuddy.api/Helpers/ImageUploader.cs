42268d0a77efe8c029cc88623a2a8a57b5e7d543
Justin Obney <justinobney@gmail.com>
2016-06-10 15:13:05 -0500

    chore: docs

--- justinobney.gymbuddy.api/Helpers/ImageUploader.cs
+++ justinobney.gymbuddy.api/Helpers/ImageUploader.cs
@@ -13,6 +13,11 @@ namespace justinobney.gymbuddy.api.Helpers
             _cloudinary = cloudinary;
         }
 
+        /// <summary>
+        /// Uploads a Base64 encoded image to a 3rd party service
+        /// </summary>
+        /// <param name="dataUri"></param>
+        /// <returns>Public Url of the uploaded image</returns>
         public string UploadFromDataUri(string dataUri)
         {
             var ms = ImageHelpers.ConvertDataUriToMemoryStream(dataUri);
