c56e3377df28a9b3e88940c14a161d587a5ff8f9
Justin Obney <justinobney@gmail.com>
2016-06-27 21:24:15 -0500

    feat(auth): associate FacebookUserId with User

--- justinobney.gymbuddy.api/Controllers/AuthController.cs
+++ justinobney.gymbuddy.api/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web.Http;
 using System.Web.Http.Description;
 using AutoMapper.QueryableExtensions;
