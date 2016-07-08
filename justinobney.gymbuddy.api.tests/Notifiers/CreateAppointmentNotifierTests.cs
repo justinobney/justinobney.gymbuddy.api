89b5694fb0146e79f3d8a1adbd24d2b0cd7273b9
Sam North <sammy.r.north@gmail.com>
2016-07-08 16:33:35 -0500

    Wrote a suiteload of tests for testing notifications based on a user's notification settings.

--- justinobney.gymbuddy.api.tests/Notifiers/CreateAppointmentNotifierTests.cs
+++ justinobney.gymbuddy.api.tests/Notifiers/CreateAppointmentNotifierTests.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using justinobney.gymbuddy.api.Data.Appointments;
 using justinobney.gymbuddy.api.Data.Devices;
 using justinobney.gymbuddy.api.Data.Gyms;
 using justinobney.gymbuddy.api.Data.Users;
+using justinobney.gymbuddy.api.Enums;
 using justinobney.gymbuddy.api.Interfaces;
 using justinobney.gymbuddy.api.Notifications;
 using justinobney.gymbuddy.api.Requests.Appointments.Create;
@@ -15,6 +17,27 @@ namespace justinobney.gymbuddy.api.tests.Notifiers
     [TestFixture]
     public class CreateAppointmentNotifierTests : BaseTest
     {
+        public User CurrentUser { get; set; }
+        public Gym DefaultGym { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            DefaultGym = new Gym { Id = 1, Name = "GloboGym" };
+            CurrentUser = new User
+            {
+                Id = 1,
+                Gender = Gender.Male,
+                FilterGender = GenderFilter.Both,
+                FitnessLevel = FitnessLevel.Intermediate,
+                FilterFitnessLevel = FitnessLevel.Beginner,
+                Gyms = new List<Gym> { DefaultGym },
+                Name = "User",
+                FacebookUserId = "1122334455"
+            };
+            Context.GetSet<User>().Attach(CurrentUser);
+            Context.GetSet<Gym>().Attach(DefaultGym);
+        }
+
         [Test]
         public void CreateAppointmentNotifier_CallsRestSharpMethod()
         {
@@ -50,24 +73,323 @@ namespace justinobney.gymbuddy.api.tests.Notifiers
 
             var request = new CreateAppointmentCommand { UserId = 1, GymId = 1 };
             var response = new Appointment { UserId = 1, User = new User { Name = "Justin" } };
-            
+
             handler.Notify(request, response);
             notifier.Received().Send(
                 Arg.Is<NotificationPayload>(x => x.Title == "New Appointment Available"),
                 Arg.Any<IEnumerable<Device>>()
                 );
+        }
+
+        [Test]
+        public void CreateAppointmentNotifier_SendsGymUserNotification()
+        {
+            var users = Context.GetSet<User>();
+            var notifier = Context.GetInstance<IPushNotifier>();
+
+            var sameGymUser = new User
+            {
+                Id = 75,
+                Gender = Gender.Male,
+                FilterGender = GenderFilter.Both,
+                FitnessLevel = FitnessLevel.Intermediate,
+                FilterFitnessLevel = FitnessLevel.Beginner,
+                Gyms = new List<Gym> { DefaultGym },
+                Name = "SAME GYM USER",
+                Devices = new List<Device>
+                {
+                    new Device { Platform = "iOS", PushToken = "98142"}
+                },
+                NewGymWorkoutNotifications = true
+            };
+
+            users.Add(sameGymUser);
+
+
+            Context.Register<IPostRequestHandler<CreateAppointmentCommand, Appointment>, CreateAppointmentNotifier>();
+            var handler = Context.GetInstance<IPostRequestHandler<CreateAppointmentCommand, Appointment>>();
+
+            var request = new CreateAppointmentCommand
+            {
+                UserId = CurrentUser.Id,
+                GymId = DefaultGym.Id,
+                Title = "Super WORKOUT",
+                TimeSlots = new List<DateTime?> { DateTime.Now }
+            };
+            var response = new Appointment { UserId = 1, User = CurrentUser, GymId = DefaultGym.Id };
+
+            handler.Notify(request, response);
 
             notifier.Received().Send(
-                Arg.Any<NotificationPayload>(),
-                Arg.Is<IEnumerable<Device>>(x=>x.Select(y=>y.PushToken).Any(t => t == "123456"))
+                Arg.Is<NotificationPayload>(x => x.Title == "New Appointment Available"),
+                Arg.Is<IEnumerable<Device>>(x => x.Select(y => y.PushToken).Any(t => t == "98142"))
                 );
+        }
+
+        [Test]
+        public void CreateAppointmentNotifier_DoesNotSendGymUserNotification()
+        {
+            var users = Context.GetSet<User>();
+            var notifier = Context.GetInstance<IPushNotifier>();
+
+            var sameGymUser = new User
+            {
+                Id = 99,
+                Gender = Gender.Male,
+                FilterGender = GenderFilter.Both,
+                FitnessLevel = FitnessLevel.Intermediate,
+                FilterFitnessLevel = FitnessLevel.Beginner,
+                Gyms = new List<Gym> { DefaultGym },
+                Name = "SAME GYM USER",
+                Devices = new List<Device>
+                {
+                    new Device { Platform = "iOS", PushToken = "554467"}
+                },
+                NewGymWorkoutNotifications = false
+            };
+
+            users.Add(sameGymUser);
+
+
+            Context.Register<IPostRequestHandler<CreateAppointmentCommand, Appointment>, CreateAppointmentNotifier>();
+            var handler = Context.GetInstance<IPostRequestHandler<CreateAppointmentCommand, Appointment>>();
+
+            var request = new CreateAppointmentCommand
+            {
+                UserId = CurrentUser.Id,
+                GymId = DefaultGym.Id,
+                Title = "Super WORKOUT",
+                TimeSlots = new List<DateTime?> { DateTime.Now }
+            };
+            var response = new Appointment { UserId = 1, User = CurrentUser, GymId = DefaultGym.Id };
+
+            handler.Notify(request, response);
+
+            notifier.DidNotReceive().Send(
+                Arg.Is<NotificationPayload>(x => x.Title == "New Appointment Available"),
+                Arg.Is<IEnumerable<Device>>(x => x.Select(y => y.PushToken).Any(t => t == "554467"))
+                );
+        }
+
+        [Test]
+        public void CreateAppointmentNotifier_SendsSquadUserNotification()
+        {
+            var users = Context.GetSet<User>();
+            var friendships = Context.GetSet<Friendship>();
+            var notifier = Context.GetInstance<IPushNotifier>();
+
+            var newNonGymUser = new User
+            {
+                Id = 57,
+                Gender = Gender.Male,
+                FilterGender = GenderFilter.Both,
+                FitnessLevel = FitnessLevel.Intermediate,
+                FilterFitnessLevel = FitnessLevel.Beginner,
+                Name = "No GYm User",
+                Devices = new List<Device>
+                {
+                    new Device { Platform = "iOS", PushToken = "877777"}
+                },
+                NewSquadWorkoutNotifications = true
+            };
+
+            var newNonSquadUser = new User
+            {
+                Id = 455,
+                Gender = Gender.Male,
+                FilterGender = GenderFilter.Both,
+                FitnessLevel = FitnessLevel.Intermediate,
+                FilterFitnessLevel = FitnessLevel.Beginner,
+                Name = "MEOW",
+                Devices = new List<Device>
+                {
+                    new Device { Platform = "iOS", PushToken = "455655122"}
+                },
+                NewSquadWorkoutNotifications = true
+            };
+
+            users.Add(newNonGymUser);
+            users.Add(newNonSquadUser);
+
+            friendships.Attach(new Friendship
+            {
+                UserId = CurrentUser.Id,
+                FriendId = newNonGymUser.Id,
+                Status = FriendshipStatus.Active,
+                FriendshipKey = "1||57",
+                Initiator = true
+            });
+            friendships.Attach(new Friendship
+            {
+                UserId = newNonGymUser.Id,
+                FriendId = CurrentUser.Id,
+                Status = FriendshipStatus.Active,
+                FriendshipKey = "1||57"
+            });
+
+            friendships.Attach(new Friendship
+            {
+                UserId = CurrentUser.Id,
+                FriendId = newNonSquadUser.Id,
+                Status = FriendshipStatus.Pending,
+                FriendshipKey = "1||455",
+                Initiator = true
+            });
+            friendships.Attach(new Friendship
+            {
+                UserId = newNonSquadUser.Id,
+                FriendId = CurrentUser.Id,
+                Status = FriendshipStatus.Pending,
+                FriendshipKey = "1||455"
+            });
+
+
+            Context.Register<IPostRequestHandler<CreateAppointmentCommand, Appointment>, CreateAppointmentNotifier>();
+            var handler = Context.GetInstance<IPostRequestHandler<CreateAppointmentCommand, Appointment>>();
+
+            var request = new CreateAppointmentCommand
+            {
+                UserId = CurrentUser.Id,
+                GymId = DefaultGym.Id,
+                Title = "Super WORKOUT",
+                TimeSlots = new List<DateTime?> { DateTime.Now }
+            };
+            var response = new Appointment { UserId = 1, User = CurrentUser, GymId = DefaultGym.Id };
+
+            handler.Notify(request, response);
 
             notifier.Received().Send(
-                Arg.Any<NotificationPayload>(),
-                Arg.Is<IEnumerable<Device>>(x => x.Select(y => y.PushToken).Any(t => t == "654321"))
+                Arg.Is<NotificationPayload>(x => x.Title == "New Appointment Available"),
+                Arg.Is<IEnumerable<Device>>(x => x.Select(y => y.PushToken).Any(t => t == "877777"))
                 );
-            
-            ConfigIoC();
+
+            notifier.DidNotReceive().Send(
+                Arg.Is<NotificationPayload>(x => x.Title == "New Appointment Available"),
+                Arg.Is<IEnumerable<Device>>(x => x.Select(y => y.PushToken).Any(t => t == "455655122"))
+                );
+        }
+
+        [Test]
+        public void CreateAppointmentNotifier_DoesNotSendSquadUserNotification()
+        {
+            var users = Context.GetSet<User>();
+            var friendships = Context.GetSet<Friendship>();
+            var notifier = Context.GetInstance<IPushNotifier>();
+
+            var newNonGymUser = new User
+            {
+                Id = 65,
+                Gender = Gender.Male,
+                FilterGender = GenderFilter.Both,
+                FitnessLevel = FitnessLevel.Intermediate,
+                FilterFitnessLevel = FitnessLevel.Beginner,
+                Name = "No GYm User",
+                Devices = new List<Device>
+                {
+                    new Device { Platform = "iOS", PushToken = "65432111"}
+                },
+                NewSquadWorkoutNotifications = false
+            };
+
+            users.Add(newNonGymUser);
+            friendships.Attach(new Friendship
+            {
+                UserId = CurrentUser.Id,
+                FriendId = newNonGymUser.Id,
+                Status = FriendshipStatus.Active,
+                FriendshipKey = "1||65",
+                Initiator = true
+            });
+            friendships.Attach(new Friendship
+            {
+                UserId = newNonGymUser.Id,
+                FriendId = CurrentUser.Id,
+                Status = FriendshipStatus.Active,
+                FriendshipKey = "1||65"
+            });
+
+
+            Context.Register<IPostRequestHandler<CreateAppointmentCommand, Appointment>, CreateAppointmentNotifier>();
+            var handler = Context.GetInstance<IPostRequestHandler<CreateAppointmentCommand, Appointment>>();
+
+            var request = new CreateAppointmentCommand
+            {
+                UserId = CurrentUser.Id,
+                GymId = DefaultGym.Id,
+                Title = "Super WORKOUT",
+                TimeSlots = new List<DateTime?> { DateTime.Now }
+            };
+            var response = new Appointment { UserId = 1, User = CurrentUser, GymId = DefaultGym.Id };
+
+            handler.Notify(request, response);
+
+            notifier.DidNotReceive().Send(
+                Arg.Is<NotificationPayload>(x => x.Title == "New Appointment Available"),
+                Arg.Is<IEnumerable<Device>>(x => x.Select(y => y.PushToken).Any(t => t == "65432111"))
+                );
+        }
+
+        [Test]
+        public void CreateAppointmentNotifier_SilenceUserNotification()
+        {
+            var users = Context.GetSet<User>();
+            var friendships = Context.GetSet<Friendship>();
+            var notifier = Context.GetInstance<IPushNotifier>();
+
+            var user = new User
+            {
+                Id = 10,
+                Gender = Gender.Male,
+                FilterGender = GenderFilter.Both,
+                FitnessLevel = FitnessLevel.Intermediate,
+                FilterFitnessLevel = FitnessLevel.Beginner,
+                Gyms = new List<Gym> { DefaultGym },
+                Name = "SAME GYM USER",
+                Devices = new List<Device>
+                {
+                    new Device { Platform = "iOS", PushToken = "99999"}
+                },
+                NewGymWorkoutNotifications = true,
+                NewSquadWorkoutNotifications = true,
+                SilenceAllNotifications = true
+            };
+
+            users.Add(user);
+            friendships.Attach(new Friendship
+            {
+                UserId = CurrentUser.Id,
+                FriendId = user.Id,
+                Status = FriendshipStatus.Active,
+                FriendshipKey = "1||10",
+                Initiator = true
+            });
+            friendships.Attach(new Friendship
+            {
+                UserId = user.Id,
+                FriendId = CurrentUser.Id,
+                Status = FriendshipStatus.Active,
+                FriendshipKey = "1||10"
+            });
+
+
+            Context.Register<IPostRequestHandler<CreateAppointmentCommand, Appointment>, CreateAppointmentNotifier>();
+            var handler = Context.GetInstance<IPostRequestHandler<CreateAppointmentCommand, Appointment>>();
+
+            var request = new CreateAppointmentCommand
+            {
+                UserId = CurrentUser.Id,
+                GymId = DefaultGym.Id,
+                Title = "Super WORKOUT",
+                TimeSlots = new List<DateTime?> { DateTime.Now }
+            };
+            var response = new Appointment { UserId = 1, User = CurrentUser, GymId = DefaultGym.Id };
+
+            handler.Notify(request, response);
+
+            notifier.DidNotReceive().Send(
+                Arg.Is<NotificationPayload>(x => x.Title == "New Appointment Available"),
+                Arg.Is<IEnumerable<Device>>(x => x.Select(y => y.PushToken).Any(t => t == "99999"))
+                );
         }
     }
 }
