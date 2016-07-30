dfa86d406c7239b3483348823cdd8cf028fe5f90
Justin Obney <justinobney@gmail.com>
2016-07-29 23:28:02 -0500

    adding transaction handling to mediatr pipeline

--- justinobney.gymbuddy.api/Requests/Decorators/TransactionHandler.cs
+++ justinobney.gymbuddy.api/Requests/Decorators/TransactionHandler.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Transactions;
 using justinobney.gymbuddy.api.Data;
 using MediatR;
+using Serilog;
+
 
 namespace justinobney.gymbuddy.api.Requests.Decorators
 {
@@ -12,22 +15,39 @@ namespace justinobney.gymbuddy.api.Requests.Decorators
         where TRequest : IRequest<TResponse>
     {
         private readonly IRequestHandler<TRequest, TResponse> _inner;
+        private readonly ILogger _log;
         private readonly AppContext _context;
 
         public TransactionHandler(
             IRequestHandler<TRequest, TResponse> inner,
+            ILogger log,
             AppContext context
             )
         {
             _inner = inner;
+            _log = log;
             _context = context;
         }
 
         public TResponse Handle(TRequest message)
         {
-            var result = _inner.Handle(message);
-            _context.SaveChanges();
-            return result;
+            _log.Information("Entering transaction handler");
+            var transactionOptions = new TransactionOptions
+            {
+                IsolationLevel = IsolationLevel.ReadCommitted
+            };
+
+            using (var transaction = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
+            {
+                var response = _inner.Handle(message);
+
+                transaction.Complete();
+                _context.SaveChanges();
+
+                _log.Information("Completed transaction");
+
+                return response;
+            }
         }
     }
 }
