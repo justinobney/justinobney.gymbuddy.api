35ce6bcaa84517dca475cebae68a0605d879d8c3
Justin Obney <justinobney@gmail.com>
2016-09-09 20:31:40 -0500

    chore: simple CreateAppointmentNotifier test update

--- justinobney.gymbuddy.api.tests/Notifiers/CreateAppointmentNotifierTests.cs
+++ justinobney.gymbuddy.api.tests/Notifiers/CreateAppointmentNotifierTests.cs
@@ -175,7 +175,7 @@ namespace justinobney.gymbuddy.api.tests.Notifiers
         }
 
         [Test]
-        public void CreateAppointmentNotifier_SendsSquadUserNotification()
+        public void CreateAppointmentNotifier_SendsNonGymSquadUserNotification()
         {
             var users = Context.GetSet<User>();
             var friendships = Context.GetSet<Friendship>();
@@ -253,11 +253,11 @@ namespace justinobney.gymbuddy.api.tests.Notifiers
             var request = new CreateAppointmentCommand
             {
                 UserId = CurrentUser.Id,
-                GymId = DefaultGym.Id,
+                Location = "Home",
                 Title = "Super WORKOUT",
                 TimeSlots = new List<DateTime?> { DateTime.Now }
             };
-            var response = new Appointment { UserId = 1, User = CurrentUser, GymId = DefaultGym.Id };
+            var response = new Appointment { UserId = 1, User = CurrentUser, Location = "Home" };
 
             handler.Notify(request, response);
 
